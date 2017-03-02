f552de6cd11ed4ad873a10f8edf3f6d899a011e0
mymyoux <mymyoux@gmail.com>
2017-03-02 16:49:46 +0100

    Add console binary

---
+++ Providers/ConsoleSupportServiceProvider.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Core\Providers;
+
+use Illuminate\Foundation\Providers\ArtisanServiceProvider;
+use Illuminate\Foundation\Providers\ComposerServiceProvider;
+use Illuminate\Support\AggregateServiceProvider;
+use Illuminate\Database\MigrationServiceProvider;
+
+class ConsoleSupportServiceProvider extends AggregateServiceProvider
+{
+    /**
+     * Indicates if loading of the provider is deferred.
+     *
+     * @var bool
+     */
+    protected $defer = true;
+
+    /**
+     * The provider class names.
+     *
+     * @var array
+     */
+    protected $providers = [
+        MigrationServiceProvider::class,
+        ComposerServiceProvider::class,
+    ];
+    protected $artisanProviders = [
+        ArtisanServiceProvider::class,
+        MigrationServiceProvider::class,
+        ComposerServiceProvider::class,
+    ];
+    public function register()
+    {
+        $this->instances = [];
+
+        $artisan = !!env('ARTISAN', True);
+        $providers = $artisan?$this->artisanProviders:$this->providers;
+        foreach ($providers as $provider) {
+            $this->instances[] = $this->app->register($provider);
+        }
+    }
+}
