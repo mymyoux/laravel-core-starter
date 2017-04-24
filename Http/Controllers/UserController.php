c41053c791d739839a079e3fe85f1bac403d8516
mymyoux <mymyoux@gmail.com>
2017-04-24 19:44:14 +0200

    list user

--- Http/Controllers/UserController.php
+++ Http/Controllers/UserController.php
@@ -11,6 +11,7 @@ use Core\Exception\ApiException;
 use \Core\Api\Paginate;
 use Core\Api\Annotations as ghost;
 use Tables\USER_LOGIN_TOKEN;
+use Tables\USER as TUSER;
   /**
    * @ghost\Role("user")
    */
@@ -46,4 +47,43 @@ class UserController extends Controller
     	$user = User::getById($id_user);
     	return $user;
     }
+    /**
+     * @ghost\Param(name="search")
+     * @ghost\Paginate(allowed="id_user,created_time,updated_time,email,first_name,last_name,login",keys="first_name",directions="1", limit=10)
+	 */
+    public function list(Request $request, Paginate $paginate)
+    {
+        $search = $request->input('search');
+        $request = User::where([]);
+        if(isset($search))
+        {
+            if(is_numeric($search))
+            {
+                $request->where(["id_user"=>$search]);
+            }else {
+                if(TUSER::hasColumn('first_name'))
+                    $request->where('first_name', 'like', '%'.$search.'%');
+                if(TUSER::hasColumn('last_name'))
+                    $request->orWhere('last_name', 'like', '%'.$search.'%');
+                if(TUSER::hasColumn('login'))
+                    $request->orWhere('login', 'like', '%'.$search.'%');
+                $request->orWhere('email', 'like', '%'.$search.'%');
+                if(strpos($search," ")!==False)
+                {
+                    if(TUSER::hasColumn('first_name') &&  TUSER::hasColumn('last_name'))
+                    {
+                        $request->orWhere(function($query) use($search)
+                        {
+                            $parts = explode(" ", $search);
+                            $first_name = array_shift($parts);
+                            $query->where('first_name', 'like', '%'.$first_name.'%');
+                            $query->where('last_name', 'like', '%'.join(" ",$parts).'%');
+                        });
+                    }
+                }
+            }
+        }
+        $request = $paginate->apply($request);
+        return $request->get();
+    } 
 }
