3f7e590910f0cf1f9debeea00de1d77b78d8817a
mymyoux <mymyoux@gmail.com>
2017-05-17 10:29:38 +0200

    events

--- Http/Controllers/EventController.php
+++ Http/Controllers/EventController.php
@@ -37,11 +37,28 @@ class EventController extends Controller
 
         //$events = Event::all();
         //$event = Event::with('owner','external')->find(1);
-        $request = Event::with('owner','external')->orderBy('id_event', 'DESC');
+        $request = Event::with('owner','external');
         $request = $paginate->apply($request, "event");
         $event = $request->get();
 
         return $event;
     } 
+    /**
+     * @ghost\Param(name="id_event",required=true,requirements="\d+",type="int")
+     * @ghost\Param(name="state",required=false)
+     * @ghost\Param(name="result",required=false)
+     * @ghost\Param(name="postpone",required=false,requirements="\d+",type="int")
+     */
+    public function answer(Request $request)
+    {
+        $event = Event::find($request->input('id_event'));
+        if(!isset($event))
+        {
+            throw new ApiException('bad_event');
+        }
+        $postpone = $request->input('postpone')?date("Y-m-d H:i:s", time()+$request->input('postpone')):NULL;
+
+        $event->answer($request->input('result'), $request->input('state'),$postpone, Auth::id());
+    }
 
 }
