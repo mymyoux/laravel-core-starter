da9ba533e6627bdea527db6f6f2bd8a44bab314d
ascheron <benjamin.andreosso@gmail.com>
2017-05-18 15:58:08 +0200

    resolved bug precision JSON model

--- Database/Eloquent/Model.php
+++ Database/Eloquent/Model.php
@@ -4,28 +4,38 @@ namespace Core\Database\Eloquent;
 
 use Illuminate\Database\Eloquent\Model as BaseModel;
 use Core\Database\Query\Builder as QueryBuilder;
+
 abstract class Model extends BaseModel
 {
     const CREATED_AT = 'created_time';
     const UPDATED_AT = 'updated_time';
 
-  protected function newBaseQueryBuilder()
- {
-    $connection = $this->getConnection();
+    public function getDateFormat()
+    {
+        return 'Y-m-d H:i:s.u';
+    }
 
-    return new QueryBuilder(
-        $connection, $connection->getQueryGrammar(), $connection->getPostProcessor()
-    );
- }
+    protected function newBaseQueryBuilder()
+    {
+        $connection = $this->getConnection();
+
+        return new QueryBuilder(
+            $connection, $connection->getQueryGrammar(), $connection->getPostProcessor()
+        );
+    }
+
     public function toRawSQL()
     {
         return $this->query->toRawSql();
     }
+
     public function toArray()
     {
         $data = parent::toArray();
+
         if(isset($this->primaryKey) && isset($data[$this->primaryKey]))
             $data["id"] = $data[$this->primaryKey];
+
         return $data;
     }
 }
