058ff981275843bb0d38bfc2d9b984930a5b0ed8
mymyoux <mymyoux@gmail.com>
2017-06-13 16:17:19 +0200

    autocomplete

---
+++ Http/views/core/components/autocomplete.vue
@@ -0,0 +1,8 @@
+<div>
+        <input spellcheck="false" @keyup="typing($event)"  @keyup.enter="enter" type="text"  data-focus value="" v-model="choice" autocomplete="off">
+        <ul class="list-scroll">
+            <li v-for="item, index in list" @click="click(item)" :class="{selected:index==selected}">
+                {{item.name}}
+            </li>
+        </ul>
+</div>
