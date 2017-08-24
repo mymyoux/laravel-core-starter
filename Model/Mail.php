f02fccfdb7f44ea61739e98aafaa5b2d11dfaa28
mymyoux <mymyoux@gmail.com>
2017-08-24 09:57:24 +0200

    Email Reject Reason

--- Model/Mail.php
+++ Model/Mail.php
@@ -9,6 +9,7 @@ use Core\Services\IP;
 use App;
 use Request;
 use Illuminate\Console\Application;
+use Db;
 class Mail extends Model
 {
     const CREATED_AT = 'created_time';
@@ -19,5 +20,13 @@ class Mail extends Model
     protected $fillable = [
         'type','id_user','subject','recipient','sender','message','from','created_time','updated_time','reason','status','id_mandrill'
     ];
-   
+    protected function getLastRejectReason($id_user)
+    {
+        $result = Mail::join('mail_webhook','mail_webhook.id_mandrill','=','mail.id_mandrill')->select(["mail_webhook.type"])->where(["id_user"=>$id_user])->whereNotIn('mail_webhook.type', ['click','open','deferral','send'])->orderBy('mail_webhook.created_time','DESC')->first();
+        if(isset($result))
+        {
+            return $result->type;
+        }
+        return NULL;
+    }
 }
