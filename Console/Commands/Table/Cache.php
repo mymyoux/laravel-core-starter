9ec0d49c3c0d571caba4729c27367a1029e328bb
mymyoux <mymyoux@gmail.com>
2017-11-01 16:25:51 +0100

    table:caceh

--- Console/Commands/Table/Cache.php
+++ Console/Commands/Table/Cache.php
@@ -47,6 +47,10 @@ class Cache extends Command
     {
         $start = microtime(True);
 
+        //TODO:change default extends for a table
+        //TODO:ignore some tables (piwik)
+        //TODO:update gitconfig
+        //TODO:remove old tables
 
         $reserved = ['__halt_compiler', 'abstract', 'and', 'array', 'as', 'break', 'callable', 'case', 'catch', 'class', 'clone', 'const', 'continue', 'declare', 'default', 'die', 'do', 'echo', 'else', 'elseif', 'empty', 'enddeclare', 'endfor', 'endforeach', 'endif', 'endswitch', 'endwhile', 'eval', 'exit', 'extends', 'final', 'for', 'foreach', 'function', 'global', 'goto', 'if', 'implements', 'include', 'include_once', 'instanceof', 'insteadof', 'interface', 'isset', 'list', 'namespace', 'new', 'or', 'parent', 'print', 'private', 'protected', 'public', 'require', 'require_once', 'return', 'static', 'switch', 'throw', 'trait', 'try', 'unset', 'use', 'var', 'while', 'xor'];
 
