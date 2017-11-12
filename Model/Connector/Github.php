0456ba55df8464fbf8e253b72f87d3675ee80547
mymyoux <mymyoux@gmail.com>
2017-11-12 16:07:48 +0100

    tables

--- Model/Connector/Github.php
+++ Model/Connector/Github.php
@@ -4,7 +4,7 @@ namespace Core\Model\Connector;
 
 use Illuminate\Database\Eloquent\Model;
 
-class Github extends Model
+class Github extends \Tables\Model\User\Connector\Github
 {
     const CREATED_AT = 'created_time';
     const UPDATED_AT = 'updated_time';
