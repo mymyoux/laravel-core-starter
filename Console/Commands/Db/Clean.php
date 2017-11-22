7c02b6485c9bfe121217feab66518341f86d2322
mymyoux <mymyoux@gmail.com>
2017-11-22 12:11:03 +0100

    better db clean

--- Console/Commands/Db/Clean.php
+++ Console/Commands/Db/Clean.php
@@ -7,6 +7,7 @@ use Tables\STATS_API_CALL;
 use Tables\ERROR;
 use Tables\STATS_WATCH;
 use Tables\BEANSTALKD_LOG;
+use Logger;
 class Clean extends CoreCommand
 {
     /**
@@ -14,7 +15,7 @@ class Clean extends CoreCommand
      *
      * @var string
      */
-    protected $signature = 'db:clean';
+    protected $signature = 'db:clean {--all=0} {--day=60}';
 
     /**
      * The console command description.
@@ -40,11 +41,43 @@ class Clean extends CoreCommand
      */
     public function handle()
     {
+        $all = $this->option('all');
+        $day = (int)$this->option('day');
+        if($all != "0" && $all!="false")
+        {
+            $all = True;
+        }else {
+            $all = False;
+        }
         $months = 2;
-        STATS_API_CALL::where("created_time","<",Db::raw("NOW() - INTERVAL $months MONTH"))->delete();
-        BEANSTALKD_LOG::where("created_time","<",Db::raw("NOW() - INTERVAL $months MONTH"))->delete();
-        ERROR::where("created_time","<",Db::raw("NOW() - INTERVAL $months MONTH"))->delete();
-        STATS_WATCH::where("created_time","<",Db::raw("NOW() - INTERVAL $months MONTH"))->delete();
+        if($day<0)
+        {
+            $day = 0;
+        }
+        Logger::warn("These tables data will be erased until: $day days");
+        Logger::info("\tstats_api_call");
+        Logger::info("\terror");
+        Logger::info("\tstats_watch");
+        if($all)
+        {
+            Logger::info("\tbeanstalkd_log");
+        }
+        if (!$this->confirm('Do you want to continue?')) {
+            Logger::info("abort");
+            return;
+        }
+        Logger::info("deleting stats_api_call");
+        STATS_API_CALL::where("created_time","<",Db::raw("NOW() - INTERVAL $day DAY"))->delete();
+        if($all)
+        {
+            Logger::info("deleting beanstalkd_log");
+            BEANSTALKD_LOG::where("created_time","<",Db::raw("NOW() - INTERVAL $day DAY"))->delete();
+        }
+        Logger::info("deleting error");
+        ERROR::where("created_time","<",Db::raw("NOW() - INTERVAL $day DAY"))->delete();
+        Logger::info("deleting stats_watch");
+        STATS_WATCH::where("created_time","<",Db::raw("NOW() - INTERVAL $day DAY"))->delete();
+        Logger::info("done");
     }
    
 }
