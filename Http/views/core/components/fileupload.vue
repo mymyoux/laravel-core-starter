c6b402b09594f01faae422b89f71182ebf2c9346
Wendy <wendy.gureghian@epitech.eu>
2018-05-02 16:36:32 +0200

    file upload component

--- Http/views/core/components/fileupload.vue
+++ Http/views/core/components/fileupload.vue
@@ -1,12 +1,8 @@
-<div class="file-upload">
-
-    <div v-if="type == 'picture'" @click="open">
-        <img v-if="!image" :src="user.getPicture(150,150)" />
-        <img v-else :src="image" data-preview />
-        <div class="cta-white-s-dark icon-photos">
-            <a href="#">(( edit_image ))</a>
-        </div>
-        <input type="file" class="picture"  @change="uploadPicture"/>
+<div class="file-upload" v-if="type == 'picture'" @click="open">
+    <img v-if="!image" :src="user.getPicture(150,150)" />
+    <img v-else :src="image" data-preview />
+    <div class="cta-white-s-dark icon-photos">
+        <a href="#">(( edit_image ))</a>
     </div>
-
+    <input type="file" class="picture"  @change="uploadPicture"/>
 </div>
