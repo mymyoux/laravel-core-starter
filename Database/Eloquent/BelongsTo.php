cba73b9164a0804f7bcdd247018f25545518b027
ascheron <benjamin.andreosso@gmail.com>
2019-02-20 20:45:06 +0100

    manage belongsto for different key IE: id_user in foreign for id (users)

--- Database/Eloquent/BelongsTo.php
+++ Database/Eloquent/BelongsTo.php
@@ -38,28 +38,85 @@ class BelongsTo extends BaseBelongsTo
                     
                     $key = $this->child->{$this->ownerKey};
                     $model = $model::find($key);
-                    foreach($wheres as $where)
+                    if ($model)
                     {
-                        $column = substr($where["column"], strlen($model->getTable()."."));
-                        if($where["operator"] == "=")
+                        foreach($wheres as $where)
                         {
-                            if($model->$column != $where["value"])
+                            $column = substr($where["column"], strlen($model->getTable()."."));
+                            if($where["operator"] == "=")
                             {
-                                return NULL;
-                            }
-                        }else
-                        if($where["operator"] == "!=" || $where["operator"] == "<>")
-                        {
-                            if($model->$column == $where["value"])
+                                if($model->$column != $where["value"])
+                                {
+                                    return NULL;
+                                }
+                            }else
+                            if($where["operator"] == "!=" || $where["operator"] == "<>")
                             {
-                                return NULL;
+                                if($model->$column == $where["value"])
+                                {
+                                    return NULL;
+                                }
                             }
                         }
                     }
                     return $model;
                 }
             }
+            else
+            {
+                // manage key if model exist && in cache
+                // IE: id_user in foreign for id (users)
+                if (isset($this->child->{$this->foreignKey}))
+                {
+                    $query = $this->query->applyScopes();
+                    $wheres = $query->getQuery()->wheres;
+                    foreach($wheres as $where)
+                    {
+                        if(!isset($where["type"]) || !isset($where["operator"]) || !isset($where["column"]) || !isset($where["value"]))
+                        {
+                            return parent::getResults();
+                        }
+                        if($where["type"] != 'Basic' || !in_array($where['operator'], ["=","!=","<>"]))
+                        {
+                            return  parent::getResults();
+                        }
+                        if(!starts_with($where["column"], $model->getTable()."."))
+                        {
+                            return parent::getResults();
+                        }
+                    }
+                    
+                    $key = $this->child->{$this->foreignKey};
+                    if ($key) {
+
+                        $model = $model::find($key);
+                        if ($model)
+                        {
+                            foreach($wheres as $where)
+                            {
+                                $column = substr($where["column"], strlen($model->getTable()."."));
+                                if($where["operator"] == "=")
+                                {
+                                    if($model->$column != $where["value"])
+                                    {
+                                        return NULL;
+                                    }
+                                }else
+                                if($where["operator"] == "!=" || $where["operator"] == "<>")
+                                {
+                                    if($model->$column == $where["value"])
+                                    {
+                                        return NULL;
+                                    }
+                                }
+                            }
+                        }
+                        return $model;
+                    }
+                }
+            }
         }
+
         return parent::getResults();
     }
 }
